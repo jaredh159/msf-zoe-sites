030b89620d0fd4ea666e1e2705069e1dc904fe20
Jared Henderson <jaredh159@users.noreply.github.com>
2025-08-22 10:44:03 -0400

    google analytics

--- src/html.rs
+++ src/html.rs
@@ -10,7 +10,21 @@ impl Html {
   pub fn new(content: &str) -> Self {
     let mut html = Self { content: content.to_string() };
     if cfg!(debug_assertions) {
-      html.content.push_str(include_str!("assets/html/refresh.html"));
+      html
+        .content
+        .push_str(include_str!("assets/html/refresh.html"));
+    } else {
+      html.content = html.content.replace(
+        "</body>",
+        "<script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+        ga('create', 'UA-2843792-2', 'auto');
+        ga('send', 'pageview');
+        </script></body>",
+      );
     }
     html
   }
